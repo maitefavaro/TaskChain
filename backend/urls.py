769dd8fce554b14e87264ca39346bca1525f95ae
maitefavaro <maitefavarodacru7z@outlook.com>
2025-04-11 19:40:13 -0300

    Adiciona rota raiz para evitar erro 404

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,13 @@
 # backend/urls.py
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import JsonResponse
+def home(request):
+    return JsonResponse({"message": "API do TaskChain está no ar!"})
 
 urlpatterns = [
+    path('', home),  # <- isso resolve o erro 404 no domínio raiz
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/", include("api.urls")),  # <--- isso inclui as rotas do app core
+    path("api/", include("api.urls")),
 ]
